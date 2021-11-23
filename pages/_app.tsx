3b11242802ad9d5fb09c3773fda37c899139b50d
Diego Machaca <diego.machaca@gmail.com>
2021-11-23 18:36:39 -0500

    chore: add layer0 for webvitals metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { Metrics } from '@layer0/rum'
+
+new Metrics({
+  token: 'b1a180b7-a5f7-4191-9a67-0af87c9b08ee'
+}).collect()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
