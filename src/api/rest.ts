d215e1952f071e690203ea645aa433a5c647ebce
Diego Machaca <diego.machaca@gmail.com>
2021-12-29 19:39:01 -0500

    fix: change unfetch for axios and eval

--- src/api/rest.ts
+++ src/api/rest.ts
@@ -1,12 +1,12 @@
 import { House } from '../model/house';
-import fetch from 'isomorphic-unfetch';
+import axios from 'axios';
 
 const baseRoot = process.env.NEXT_PUBLIC_API_URI;
 const storeCollectionURL = `${baseRoot}/resume`
 
 export const fetchStores = async (): Promise<House[]> => {
-  const res = await fetch(storeCollectionURL)
-  const data = await res.json();
+  const res = await axios.get(storeCollectionURL);
+  let data = eval(res.data);
   return data.map(
     ({ compra, tienda, url, venta, image, fecha }: any) => ({ compra, tienda, url, venta, image, fecha } as House)
   );
