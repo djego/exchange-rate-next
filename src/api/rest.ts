8500df9e3213c708bddaf5e4dc9e27b7209a583e
Diego Machaca <diego.machaca@gmail.com>
2022-04-05 23:33:29 -0500

    feat: use next/image and recovery moment time

--- src/api/rest.ts
+++ src/api/rest.ts
@@ -5,9 +5,8 @@ const baseRoot = process.env.NEXT_PUBLIC_API_URI;
 const storeCollectionURL = `${baseRoot}/resume`
 
 export const fetchStores = async (): Promise<House[]> => {
-  const res = await axios.get(storeCollectionURL);
-  let data = eval(res.data);
-  return data.map(
+  const response = await axios.get(storeCollectionURL);
+  return response.data.map(
     ({ compra, tienda, url, venta, image, fecha }: any) => ({ compra, tienda, url, venta, image, fecha } as House)
   );
 }
--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,5 +1,6 @@
 import { House } from '../../interfaces/house';
 import styles from '../../styles/Home.module.css';
+import Image from "next/image";
 import Moment from 'react-moment';
 import 'moment/locale/es';
 
@@ -13,14 +14,14 @@ export const ListHouse = ({ houses }: Props) => {
       <div className={styles.grid}>
       { houses.map( (house, id) => 
         <a href={house.url} key={id} className={styles.card} target="_blank" rel="noreferrer" > 
-          <h2><img src={house.image} width="16" height="16" alt={house.tienda} /> {house.tienda}</h2>
+          <h2><Image src={house.image} width="16" height="16" alt={house.tienda} /> {house.tienda}</h2>
           <p>Compra: <b>{house.compra}</b></p>
           <p>Venta: <b>{house.venta}</b></p>
         </a>
       )}
       </div>
       <div className={styles.bottom}>
-      <span>Se actualiza cada 10 minutos</span>
+      <span>Actualizado:&nbsp;<Moment locale="es" fromNow>{houses[0].fecha}</Moment></span>
       </div>      
     </div>
 
